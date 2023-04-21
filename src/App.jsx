08f5be1dc4436c8b3232bdb9bcea4145d4b10c80
FlaceyFurr <gontiefox@gmail.com>
2023-04-21 19:02:55 +0300

    Added router to game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Classic from "./pages/classic/Classic";
+import Bonus from "./pages/bonus/Bonus";
 import "./App.css";
 
 function App() {
-
   return (
-    <div className="App">
-
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/classic" element={<Classic />} />
+          <Route path="/bonus" element={<Bonus />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
