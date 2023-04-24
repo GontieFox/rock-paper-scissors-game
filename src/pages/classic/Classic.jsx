71e93e52ed70caadc89b572c593aaf4566d6ab9e
FlaceyFurr <gontiefox@gmail.com>
2023-04-24 12:59:37 +0300

    Added rules popup

--- src/pages/classic/Classic.jsx
+++ src/pages/classic/Classic.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import "./index.css";
 import logo from "./../../assets/images/logo.svg";
 import triangle from "./../../assets/images/bg-triangle.svg";
 import paper from "./../../assets/images/icon-paper.svg";
 import scissors from "./../../assets/images/icon-scissors.svg";
 import rock from "./../../assets/images/icon-rock.svg";
+import rules from "./../../assets/images/image-rules.svg";
 
 const Classic = () => {
+    const [popupOpened, setPopupOpened] = useState(false);
+
+    const handlePopupOpened = () => {
+        setPopupOpened(!popupOpened);
+    }
+
     return (
         <>
+            <div className={`${popupOpened ? "popup-rules_opened" : ""} popup-rules`} id="rules">
+                <div className="popup-rules__container">
+                    <div className="popup-rules__section">
+                        <h2 className="popup-rules__title">Rules</h2>
+                        <button className="popup-rules__close-btn" id="close-btn" onClick={handlePopupOpened} />
+                    </div>
+                    <img src={rules} alt="rules" className="popup-rules__image" />
+                </div>
+            </div>
+
             <div className="border">
                 <img src={logo} alt="logo" className="rules-logo" />
                 <div className="score">
@@ -15,6 +33,7 @@ const Classic = () => {
                     <h1 className="score__count">12</h1>
                 </div>
             </div>
+
             <div className="game">
                 <button className="btn btn-paper">
                     <div className="btn__background">
@@ -33,7 +52,7 @@ const Classic = () => {
                 </button>
                 <img src={triangle} alt="triangle" className="game__triangle" />
             </div>
-            <button className="rules-btn">Rules</button>
+            <button className="rules-btn" id="rules-btn" onClick={handlePopupOpened}>Rules</button>
         </>
     );
 }
